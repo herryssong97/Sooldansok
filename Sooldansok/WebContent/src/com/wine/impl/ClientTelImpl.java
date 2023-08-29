02460692953621560dbbd98cb7083bfb2864e70e
herryssong97 <76979784+herryssong97@users.noreply.github.com>
2023-08-29 16:27:33 +0900

    Delete Sooldansok/WebContent/src directory

--- Sooldansok/WebContent/src/com/wine/impl/ClientTelImpl.java
+++
@@ -1,8 +0,0 @@
-package com.wine.impl;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface ClientTelImpl {
-	public void clienttel(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
--- Sooldansok/WebContent/src/com/wine/impl/JoinTelImpl.java
+++
@@ -1,8 +0,0 @@
-package com.wine.impl;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface JoinTelImpl {
-	public boolean jointelImpl(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
